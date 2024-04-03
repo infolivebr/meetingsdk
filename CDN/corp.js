08149448d500f57fb29388f018aa6ca5e18d9428
Gilmar <eu@gilmar.dev>
2024-04-03 15:10:51 -0300

    Adicionando client_id e client_secret

--- CDN/corp.js
+++ CDN/corp.js
@@ -33,7 +33,7 @@ new WebpackDevServer(
     proxy: [
       {
         path: '/meeting.html',
-        target: 'https://meet.infolive.digital/meeting/'
+        // target: 'http://127.0.0.1:9998/'
       }
     ],
     static: './',
